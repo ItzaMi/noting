919e9bb637bfedce5640f1c4252a02b38736ccf0
ItzaMi <rros00@gmail.com>
2020-12-01 20:08:11 +0000

    feat(App.js) now adds notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([{ text: 'Hello' }]);
+  const [note, setNote] = useState('');
+
+  const passNoteToState = ({ target }) => {
+    setNote(target.value);
+  };
+
+  const addNote = () => {
+    setData((data) => [...data, { text: note }]);
+    setNote('');
+  };
+
+  const addNoteWithEnter = (event) => {
+    if (event.keyCode === 13) {
+      addNote();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <button class="btn btn-green shadow-2xl">Button</button>
-      </header>
+    <div>
+      <input
+        id="noteinput"
+        style={{ width: '80%' }}
+        type="text"
+        placeholder="Enter a new note"
+        value={note}
+        onChange={passNoteToState}
+        onKeyDown={(e) => addNoteWithEnter(e)}
+      />
+      <button onClick={() => addNote()}>Add note</button>
+      {data.map((note) => (
+        <p key={note.text}>{note.text}</p>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
