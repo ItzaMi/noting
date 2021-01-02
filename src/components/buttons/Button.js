c5d0a27ba958c74b76ffb2b9c44d269c7172facc
ItzaMi <rros00@gmail.com>
2021-01-02 15:22:20 -0100

    feat(Button) conditional classNames

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,7 +1,12 @@
 const Button = ({ text, onClickAction, type }) => {
   return (
     <button
-      className="py-2 px-5 w-full bg-gray-700 text-white hover:bg-gray-900 transition-colors duration-200 ease-in-out"
+      className={`py-2 px-5 w-full text-white transition-colors duration-200 ease-in-out +
+        ${
+          type === 'save'
+            ? 'bg-gray-700 hover:bg-gray-900'
+            : 'bg-blue-700 hover:bg-blue-900'
+        }`}
       onClick={onClickAction}
     >
       {text}
