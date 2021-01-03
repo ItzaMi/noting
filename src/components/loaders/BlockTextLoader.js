b239f553487b09e14f88c1e28892550a53302ecc
ItzaMi <rros00@gmail.com>
2021-01-03 12:45:14 -0100

    feat(BlockTextLoader) change name from TextLoader

--- src/components/loaders/BlockTextLoader.js
+++ src/components/loaders/BlockTextLoader.js
@@ -1,4 +1,4 @@
-const TextLoader = () => (
+const BlockTextLoader = () => (
   <div className="h-screen bg-gray-100 py-5 px-7">
     <div className="w-5/12 h-3 bg-gradient-to-r from-gray-200 to-gray-300 rounded-full animate-pulse"></div>
     <div className="w-3/12 h-3 mt-2 bg-gradient-to-r from-gray-200 to-gray-300 rounded-full animate-pulse"></div>
@@ -6,4 +6,4 @@ const TextLoader = () => (
   </div>
 );
 
-export default TextLoader;
+export default BlockTextLoader;
