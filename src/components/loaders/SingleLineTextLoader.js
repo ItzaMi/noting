b387697334e4366f24f8d42dd512b2d5c919adb9
ItzaMi <rros00@gmail.com>
2021-01-03 12:45:26 -0100

    feat(loaders) SingleLineTextLoader

---
+++ src/components/loaders/SingleLineTextLoader.js
@@ -0,0 +1,5 @@
+const SingleLineTextLoader = () => (
+  <div className="w-6/12 h-5 mt-1 mb-1 bg-gradient-to-r from-gray-200 to-gray-300 rounded-full animate-pulse"></div>
+);
+
+export default SingleLineTextLoader;
