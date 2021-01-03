0f15472db2fb5840fb00f47b9e564b33e5647d2b
ItzaMi <rros00@gmail.com>
2021-01-03 12:29:34 -0100

    feat(loaders) TextLoader

---
+++ src/components/loaders/TextLoader.js
@@ -0,0 +1,9 @@
+const TextLoader = () => (
+  <div className="h-screen bg-gray-100 py-5 px-7">
+    <div className="w-5/12 h-3 bg-gradient-to-r from-gray-200 to-gray-300 rounded-full animate-pulse"></div>
+    <div className="w-3/12 h-3 mt-2 bg-gradient-to-r from-gray-200 to-gray-300 rounded-full animate-pulse"></div>
+    <div className="w-4/12 h-3 mt-2 bg-gradient-to-r from-gray-200 to-gray-300 rounded-full animate-pulse"></div>
+  </div>
+);
+
+export default TextLoader;
