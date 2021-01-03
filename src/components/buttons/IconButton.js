d1d434e50a4ccbd0ddddf00047ef44e56fa70c9b
ItzaMi <rros00@gmail.com>
2021-01-03 19:46:51 -0100

    feat(IconButton) condition for darkMode

--- src/components/buttons/IconButton.js
+++ src/components/buttons/IconButton.js
@@ -1,12 +1,20 @@
 import Edit from '../icons/Edit';
 import View from '../icons/View';
 import Delete from '../icons/Delete';
+import Moon from '../icons/Moon';
+import Sun from '../icons/Sun';
 
 import styles from '../icons/icons.module.css';
 
-const IconButton = ({ onClickAction, typeOfIcon }) => {
+const IconButton = ({ onClickAction, typeOfIcon, darkMode }) => {
   const checkTypeOfIcon = (typeOfIcon) => {
     switch (typeOfIcon) {
+      case 'dark':
+        if (darkMode) {
+          return <Sun className={styles.sunIcon} />;
+        } else {
+          return <Moon className={styles.moonIcon} />;
+        }
       case 'edit':
         return <Edit className={styles.editIcon} />;
       case 'delete':
@@ -18,7 +26,20 @@ const IconButton = ({ onClickAction, typeOfIcon }) => {
   };
 
   return (
-    <button className="mr-2 h-20px" onClick={onClickAction}>
+    <button
+      className={`${
+        typeOfIcon === 'dark'
+          ? 'w-40px h-40px flex flex-row justify-center items-center'
+          : 'mr-2 h-20px'
+      } ${
+        typeOfIcon === 'dark' && darkMode
+          ? 'bg-gray-700'
+          : typeOfIcon === 'dark' && !darkMode
+          ? 'bg-gray-300'
+          : null
+      }`}
+      onClick={onClickAction}
+    >
       {checkTypeOfIcon(typeOfIcon)}
     </button>
   );
