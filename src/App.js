efbe584312478df153e221c30ab429c0baa00f97
ItzaMi <rros00@gmail.com>
2021-01-03 12:31:14 -0100

    feat(App) loading state + setting up left panel content

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Error from './components/toast/Error';
 import Button from './components/buttons/Button';
 import MarkdownPanel from './components/markdownPanel/MarkdownPanel';
 import Note from './components/note/Note';
+import TextLoader from './components/loaders/TextLoader';
 
 const App = () => {
   const [data, setData] = useState(() => {
@@ -23,18 +24,20 @@ const App = () => {
       : defaultValue;
   });
 
-  const [note, setNote] = useState('Start your new note here');
+  const [loading, setLoading] = useState(true);
+  const [note, setNote] = useState(() => {
+    if (loading) {
+      return '';
+    } else {
+      return 'Start your new note here';
+    }
+  });
   const [viewMode, setViewMode] = useState(false);
   const [editMode, setEditMode] = useState(false);
   const [noteToEdit, setNoteToEdit] = useState('');
   const [noteToBeViewed, setNoteToBeViewed] = useState('');
   const [error, setError] = useState(false);
   const [editorHeight, setEditorHeight] = useState(window.innerHeight);
-
-  const addNote = () => {
-    setData((data) => [...data, { text: note, noteIsInViewMode: false }]);
-    setNote('Start your new note here');
-  };
 
   useEffect(() => {
     localStorage.setItem('data', JSON.stringify(data));
@@ -44,6 +47,40 @@ const App = () => {
     window.addEventListener('load', updateMDEditorHeight);
     window.addEventListener('resize', updateMDEditorHeight);
   });
+
+  const viewNote = (noteSetToView) => {
+    if (loading) {
+      setLoading(false);
+    }
+    if (editMode) {
+      setError(true);
+      return;
+    }
+    const getNoteWithViewModeTrue = data.filter(
+      (note) => note.noteIsInViewMode === true
+    );
+    if (getNoteWithViewModeTrue.length) {
+      getNoteWithViewModeTrue[0].noteIsInViewMode = false;
+    }
+    setViewMode(true);
+    noteSetToView.noteIsInViewMode = true;
+    setNoteToBeViewed(noteSetToView.text);
+  };
+
+  useEffect(() => {
+    if (data.length > 0) {
+      const firstNoteAvailable = data[0];
+      viewNote(firstNoteAvailable);
+    } else {
+      setLoading(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const addNote = () => {
+    setData((data) => [...data, { text: note, noteIsInViewMode: false }]);
+    setNote('Start your new note here');
+  };
 
   const deleteNote = (noteToDelete) => {
     if (editMode) {
@@ -59,7 +96,9 @@ const App = () => {
     noteToChange[0].text = newTextNote;
     setEditMode(false);
     noteToChange[0].isBeingEdited = false;
-    setNote('Start your new note here');
+    setViewMode(true);
+    noteToChange[0].noteIsInViewMode = true;
+    viewMode(noteToChange[0]);
     localStorage.setItem('data', JSON.stringify(data));
   };
 
@@ -86,40 +125,43 @@ const App = () => {
     setNoteToEdit(noteToBeEdit.text);
   };
 
-  const viewNote = (noteSetToView) => {
-    if (editMode) {
-      setError(true);
-      return;
-    }
+  const newNote = () => {
+    setViewMode(false);
     const getNoteWithViewModeTrue = data.filter(
       (note) => note.noteIsInViewMode === true
     );
     if (getNoteWithViewModeTrue.length) {
       getNoteWithViewModeTrue[0].noteIsInViewMode = false;
     }
-    setViewMode(true);
-    noteSetToView.noteIsInViewMode = true;
-    setNoteToBeViewed(noteSetToView.text);
+    setNote('Start your new note here');
   };
 
   return (
     <div className="relative bg-gray-100 h-screen flex flex-row">
       <div className="w-9/12">
-        <MarkdownPanel
-          viewMode={viewMode}
-          editorHeight={editorHeight}
-          noteToBeViewed={noteToBeViewed}
-          note={note}
-          setNote={setNote}
-        />
+        {loading ? (
+          <TextLoader />
+        ) : (
+          <MarkdownPanel
+            viewMode={viewMode}
+            editorHeight={editorHeight}
+            noteToBeViewed={noteToBeViewed}
+            note={note}
+            setNote={setNote}
+          />
+        )}
       </div>
       <div className="w-3/12 bg-gray-200 h-screen overflow-scroll">
-        {editMode ? (
+        {loading ? (
+          <Button text="" type="loading" />
+        ) : editMode ? (
           <Button
             text="Save changes"
             onClickAction={() => saveChanges(noteToEdit, note)}
             type="save"
           />
+        ) : viewMode ? (
+          <Button text="New note" onClickAction={() => newNote()} type="new" />
         ) : (
           <Button text="Add note" onClickAction={() => addNote()} type="add" />
         )}
