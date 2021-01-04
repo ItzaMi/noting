2632d6e8050acd9ea94419ab8bce73e7d2480a7e
ItzaMi <rros00@gmail.com>
2021-01-04 11:55:05 -0100

    feat(Button) darkMode styling

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -6,7 +6,7 @@ const Button = ({ text, onClickAction, type }) => {
       className={`py-2 px-5 w-full text-white transition-colors duration-200 ease-in-out
         ${
           type === 'save'
-            ? 'bg-gray-700 hover:bg-gray-900'
+            ? 'bg-gray-700 hover:bg-gray-900 dark:bg-gray-600 dark:hover:bg-gray-900'
             : 'bg-blue-700 hover:bg-blue-900'
         } ${type === 'loading' && 'flex flex-row justify-center'}`}
       onClick={onClickAction}
